05865ca6947807aae61df396f92dedecba93fcfc
virmos <hadonggiang1810@gmail.com>
2022-01-14 16:33:48 +0700

    update readme

---
+++ preliminary/lab3_handout/temp/test.c
@@ -0,0 +1,9 @@
+#include <limits.h>
+#include <stdio.h>
+int main(void) {
+ // Report the range of `char` on this system
+ printf("CHAR_MIN = %d\n"
+ "CHAR_MAX = %d\n",
+ CHAR_MIN, CHAR_MAX);
+ return 0;
+}
